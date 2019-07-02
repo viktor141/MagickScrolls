8f88eb7aad30ecc9c20c6a6f1618897a32551f24
viktor141 <31136336+viktor141@users.noreply.github.com>
2019-07-02 19:44:43 +0400

    start

---
+++ src/ru/vixtor141/MagickScrolls/Crafts.java
@@ -0,0 +1,40 @@
+package ru.vixtor141.MagickScrolls;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Crafts {
+
+    public static void crafts(){
+        scrollOfTeleportation();
+
+    }
+
+    private static void scrollOfTeleportation(){
+        ItemStack scrollOfTeleportation = new ItemStack(Material.PAPER);
+        ItemMeta meta = scrollOfTeleportation.getItemMeta();
+        meta.setDisplayName(ChatColor.DARK_PURPLE+ "Scroll of teleportation");
+        List<String> lore = new ArrayList<String>();
+        lore.add("Scroll for teleportation in dimension");
+        meta.setLore(lore);
+        meta.addEnchant(Enchantment.DAMAGE_ALL, 3,true);
+        scrollOfTeleportation.setItemMeta(meta);
+
+        ShapedRecipe s = new ShapedRecipe(scrollOfTeleportation);
+        s.shape(new String[]{"E E"," P ","E E"});
+        s.setIngredient('E', Material.ENDER_PEARL);
+        s.setIngredient('P', Material.PAPER);
+        Bukkit.getServer().addRecipe(s);
+    }
+
+
+
+}
---
+++ src/ru/vixtor141/MagickScrolls/Main.java
@@ -0,0 +1,33 @@
+package ru.vixtor141.MagickScrolls;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftShapedRecipe;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.vixtor141.MagickScrolls.Crafts.crafts;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info(ChatColor.YELLOW+"Plugin was enabled");
+        crafts();
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info(ChatColor.GOLD+"Plugin was disabled");
+    }
+
+
+
+}
