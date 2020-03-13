bc6198f6ad4925e4782d20cc5003ab6d660572f9
viktor141 <gomolitskiy@mail.ru>
2020-03-13 07:07:01 +0400

    Added async task for vortex scroll

--- src/ru/vixtor141/MagickScrolls/events/VortexScroll.java
+++ src/ru/vixtor141/MagickScrolls/events/VortexScroll.java
@@ -1,5 +1,6 @@
 package ru.vixtor141.MagickScrolls.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -8,12 +9,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityInteractEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import ru.vixtor141.MagickScrolls.CDSystem;
+import ru.vixtor141.MagickScrolls.Main;
 import ru.vixtor141.MagickScrolls.Mana;
 
 import java.util.Collection;
@@ -21,39 +22,28 @@ import java.util.Collection;
 import static java.lang.Math.*;
 import static java.lang.Math.toRadians;
 
-public class VortexScroll implements Listener {
+public class VortexScroll implements Listener, Runnable {
+
+    private Player player;
+    private ItemStack item;
 
     @EventHandler
     public void use(PlayerInteractEvent event) {
         if(event.getHand().equals(EquipmentSlot.OFF_HAND))return;
         if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if (event.getPlayer().getInventory().getItemInMainHand().getType() != Material.PAPER) return;
-        ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
+        item = event.getPlayer().getInventory().getItemInMainHand();
         if (!item.getItemMeta().hasLore()) return;
         if (!item.getItemMeta().getLore().get(0).equals("Vortex scroll")) return;
 
-        Player player = event.getPlayer();
+        player = event.getPlayer();
         event.setCancelled(true);
 
-        Entity targetEntity = searchEntity(player);
-
-        if(targetEntity == null){
-            player.sendMessage("Target not defined");
-            return;
-        }
-
-        Mana playerMana = Mana.getPlayerMap().get(player);
-        if(!playerMana.getCdSystem().CDStat(CDSystem.Scrolls.VORTEX, playerMana, 20, 30))return;
-
-        targetEntity.setVelocity(new Vector(0,1.9,0));
-
-        if(!player.getGameMode().equals(GameMode.CREATIVE)) {
-            item.setAmount(item.getAmount() - 1);
-            player.getInventory().setItemInMainHand(item);
-        }
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), this);
     }
 
-    public Entity searchEntity(Player player){
+    @Override
+    public void run() {
         Location locationOfPlayer = player.getLocation();
         Location newLocation;
 
@@ -72,16 +62,33 @@ public class VortexScroll implements Listener {
                 for(Entity entity : collectionEntities){
                     if(entity instanceof Player){
                         if((Player)entity != player){
-                            return entity;
+                            entitySetVelocity(entity);
+                            return;
                         }
                     }else{
-                        return entity;
+                        entitySetVelocity(entity);
+                        return;
                     }
                 }
             }
         }
-        return null;
+        entitySetVelocity(null);
     }
 
+    private void entitySetVelocity(Entity targetEntity){
+        if(targetEntity == null){
+            player.sendMessage("Target not defined");
+            return;
+        }
 
+        Mana playerMana = Mana.getPlayerMap().get(player);
+        if(!playerMana.getCdSystem().CDStat(CDSystem.Scrolls.VORTEX, playerMana, 20, 30))return;
+
+        targetEntity.setVelocity(new Vector(0,1.9,0));
+
+        if(!player.getGameMode().equals(GameMode.CREATIVE)) {
+            item.setAmount(item.getAmount() - 1);
+            player.getInventory().setItemInMainHand(item);
+        }
+    }
 }
