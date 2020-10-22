3d54b1ba6fdebf229e743e189c526f33f1a7455e
viktor141 <gomolitskiy@mail.ru>
2020-10-22 19:38:09 +0400

    Added particles to earth scroll

--- src/ru/vixtor141/MagickScrolls/scrolls/Earth.java
+++ src/ru/vixtor141/MagickScrolls/scrolls/Earth.java
@@ -12,6 +12,7 @@ import org.bukkit.util.Vector;
 import ru.vixtor141.MagickScrolls.CDSystem;
 import ru.vixtor141.MagickScrolls.Main;
 import ru.vixtor141.MagickScrolls.Mana;
+import ru.vixtor141.MagickScrolls.effects.RandomParticleGenerator;
 import ru.vixtor141.MagickScrolls.interfaces.Scroll;
 
 import static java.lang.Math.*;
@@ -64,6 +65,7 @@ public class Earth implements Scroll, Runnable {
             fallingBlock.setVelocity(new Vector(0, 1.1, 0));
             fallingBlock.setDropItem(false);
             fallingBlock.setMetadata("magickscrolls_earthScoll", new LazyMetadataValue(Main.getPlugin(), () -> player));
+            dustEffect(start.clone().add(x * ((double)-1), 1, z));
             start.getWorld().getNearbyEntities(start.clone().add(x * ((double)-1), 0, z ), 2, 2,2).parallelStream().filter(entity -> !entity.getType().equals(EntityType.FALLING_BLOCK) && !entity.equals(player)).forEach(entity -> entity.setVelocity(dir.clone().add(new Vector(0,1.3,0)).multiply(1.5)));
         }
 
@@ -71,5 +73,9 @@ public class Earth implements Scroll, Runnable {
         if(i >= r){
             earthScrollTask.cancel();
         }
+    }
+
+    private void dustEffect(Location location){
+        new RandomParticleGenerator(location,10, 3, (double)102/255, (double)51/255, 0.0);
     }
 }
